8e214f4222c758eff671c11f93257bddf7d1c582
artur-h <artur.hlushko@gmail.com>
2020-03-30 21:32:54 +0300

    Ability to delete items (using promises)

--- index.js
+++ index.js
@@ -1,25 +1,59 @@
-const fruits = [
+let fruits = [
   {id: 1, title: 'Apples', price: 20, img: 'https://e1.edimdoma.ru/data/ingredients/0000/2374/2374-ed4_wide.jpg?1487746348'},
   {id: 2, title: 'Oranges', price: 30, img: 'https://fashion-stil.ru/wp-content/uploads/2019/04/apelsin-ispaniya-kg-92383155888981_small6.jpg'},
   {id: 3, title: 'Mango', price: 40, img: 'https://itsfresh.ru/upload/iblock/178/178d8253202ef1c7af13bdbd67ce65cd.jpg'},
 ];
 
-const modal = $.modal({
-  title: 'Modal',
+const toHTML = fruit => `
+  <div class="col">
+    <div class="card">
+      <img style="height: 300px" src="${fruit.img}" class="card-img-top" alt="${fruit.title}">
+      <div class="card-body">
+        <h5 class="card-title">${fruit.title}</h5>
+        <a href="#" class="btn btn-primary" data-btn="price" data-id="${fruit.id}">What's the price?</a>
+        <a href="#" class="btn btn-danger" data-btn="remove" data-id="${fruit.id}">Delete</a>
+      </div>
+    </div>
+  </div>
+`;
+
+function render() {
+  document.getElementById('fruits').innerHTML = fruits.map(toHTML).join('');
+}
+
+render();
+
+const priceModal = $.modal({
+  title: "Product's price",
   closable: true,
-  content: `
-    <p>lorem ipsum dolor sit.</p>
-    <p>lorem ipsum dolor sit.</p>
-  `,
   width: '400px',
   footerButtons: [
-    {text: 'Ok', type: 'primary', handler() {
-      console.log('Primary btn clicked');
-      modal.close();
-    }},
-    {text: 'Cancel', type: 'danger', handler() {
-      console.log('Danger btn clicked');
-      modal.close();
-    }},
+    {text: 'Close', type: 'primary', handler() {
+      priceModal.close();
+    }}
   ]
+});
+
+document.addEventListener('click', event => {
+  event.preventDefault();
+  const btnType = event.target.dataset.btn;
+  const id = +event.target.dataset.id;
+  const fruit = fruits.find(f => f.id === id);
+
+  if (btnType === 'price') {
+    priceModal.setContent(`
+      <p>The price for ${fruit.title} is: <strong>$${fruit.price}</strong></p>
+    `);
+    priceModal.open();
+  } else if (btnType === 'remove') {
+    $.confirm({
+      title: 'Are you sure?',
+      content: `<p>You are deleting: <strong>${fruit.title}</strong></p>`
+    }).then(() => {
+      fruits = fruits.filter(f => f.id !== id);
+      render();
+    }).catch(() => {
+      console.log('cancel');
+    })
+  }
 });
